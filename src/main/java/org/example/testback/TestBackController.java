a95e720dfa0dcac59f943eb971939b66204ee702
thay123028 <thay123028@naver.com>
2025-09-10 17:45:54 +0900

    test

--- src/main/java/org/example/testback/TestBackController.java
+++ src/main/java/org/example/testback/TestBackController.java
@@ -11,7 +11,7 @@ public class TestBackController {
 
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.status(200).body("바이바이..");
+        return ResponseEntity.status(200).body("다시..");
     }
 
     @GetMapping("/health")
