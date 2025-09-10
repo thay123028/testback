9d1a5d4d6ff60b8125abf32e4f41b471912e26bd
thay123028 <thay123028@naver.com>
2025-09-10 15:48:31 +0900

    test

--- src/main/java/org/example/testback/TestBackController.java
+++ src/main/java/org/example/testback/TestBackController.java
@@ -1,6 +1,7 @@
 package org.example.testback;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestBackController {
 
+    @GetMapping
     public ResponseEntity test() {
         return ResponseEntity.status(200).body("바이..");
     }
